125a579c3b816e660bb7ccc873ef309d5b13da44
NikitaGundorin <djnig@yandex.ru>
2020-10-19 22:03:21 +0300

    Add app icon

--- RideAndFood/LoginScreen/LoginViewController.swift
+++ RideAndFood/LoginScreen/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  RideAndFood
 //
 //  Created by Nikita Gundorin on 19.10.2020.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
 
 }
 
