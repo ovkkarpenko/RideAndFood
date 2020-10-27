6e5f98d1840e1001a1ba5c3e0e6faa001f530917
NikitaGundorin <djnig@yandex.ru>
2020-10-27 22:47:26 +0300

    Merge conflict resolve

---
+++ RideAndFood/SupportingFiles/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  RideAndFood
+//
+//  Created by Nikita Gundorin on 19.10.2020.
+//  Copyright © 2020 skillbox. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // MARK: - Public properties
+    
+    var window: UIWindow?
+
+    // MARK: - App lifecycle methods
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let storyboard = UIStoryboard(name: "Settings", bundle: nil)
+        let rootVc = storyboard.instantiateInitialViewController()
+        
+        window = UIWindow()
+        window?.rootViewController = rootVc
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
