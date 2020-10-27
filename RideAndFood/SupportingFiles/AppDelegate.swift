aa592e0b5e2b9a83dc01e80f765a60cf49a199cc
NikitaGundorin <djnig@yandex.ru>
2020-10-27 22:20:27 +0300

    Add test view controller

--- RideAndFood/SupportingFiles/AppDelegate.swift
+++ RideAndFood/SupportingFiles/AppDelegate.swift
@@ -19,7 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        let rootVc = LoginViewController()
+//        let rootVc = LoginViewController()
+        let rootVc = ViewController()
         
         window = UIWindow()
         window?.rootViewController = rootVc
